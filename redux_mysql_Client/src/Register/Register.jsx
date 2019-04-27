673f0ddbe8d4864da969ba19f362922b2f5b558d
Guru <gurubrahmac@gmail.com>
2019-04-27 23:48:18 +0530

    Done the Registration Page

--- redux_mysql_Client/src/Register/Register.jsx
+++ redux_mysql_Client/src/Register/Register.jsx
@@ -1,10 +1,26 @@
 import "./Register.css";
 import React from "react";
+import {Field} from 'redux-form';
+import textBox from '../reduxforms/textBox';
+
 
 function template() {
+  const {handleSubmit,invalid,reset}=this.props
   return (
     <div className="register">
-      <h1>Register</h1>
+      <h1 className='text-center'>Register</h1>
+      <form onSubmit={handleSubmit(this.fnRegister)}>
+      <Field type='text' name="uid" lbl="User Name" component={textBox}/>
+      <Field type='password' name="pwd" lbl= "Password" component={textBox}/>
+      <Field type='email' name="email" lbl="Email" component={textBox}/>
+      <Field type='phone' name="phone" lbl= "Phone" component={textBox}/>
+      <div className='row'>
+      <div className='offset-sm-5 col-sm-7'>
+      <input disabled={invalid} type="submit" value="Register" className="btn btn-primary"/>
+      <input onClick={reset} type="button" value="Reset" className="btn btn-primary"/>
+      </div>            
+      </div>
+      </form>
     </div>
   );
 };
